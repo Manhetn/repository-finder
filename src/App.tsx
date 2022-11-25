aedbed5055453ed47aab8daf90b8dda4bd1b7359
Ilya Shestakov <ilya.shestakov.md@gmail.com>
2022-11-25 13:56:37 +0500

    made basic project setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
+import FavoritesPage from './pages/FavoritesPage/FavoritesPage';
+import HomePage from './pages/HomePage/HomePage';
+import SearchPage from './pages/SearchPage/SearchPage';
 
 function App() {
   return (
-   <>
-    <h1>App</h1>
-   </>
+    <>
+      <h1>App</h1>
+      <Switch>
+        <Route path="/search" component={SearchPage} />
+        <Route path="/favorites" component={FavoritesPage} />
+        <Route path="/" component={HomePage} />
+        <Redirect to="/" />
+      </Switch>
+    </>
   );
 }
 
---
+++ src/pages/FavoritesPage/FavoritesPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FavoritesPage = () => {
+  return (
+    <>
+      <h1>FavoritesPage</h1>
+    </>
+  );
+};
+
+export default FavoritesPage;
